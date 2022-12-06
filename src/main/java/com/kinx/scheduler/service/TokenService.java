f2fc4365964f58da6d2d5ddf187c45d0b166af66
DESKTOP-18053R8\김예슬 <kys912@kinx.net>
2022-12-06 18:03:37 +0900

    study webClient,Webflux

--- src/main/java/com/kinx/scheduler/service/TokenService.java
+++ src/main/java/com/kinx/scheduler/service/TokenService.java
@@ -1,4 +1,13 @@
 package com.kinx.scheduler.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
 public class TokenService {
+    private final WebClient webClient;
+
+    public TokenService(WebClient.Builder webClientBuilder){
+
+    }
 }
