d290b0c2a160eae2769d40c9dc548222b712f9a9
DESKTOP-18053R8\김예슬 <kys912@kinx.net>
2022-12-13 17:45:45 +0900

    response structure

---
+++ src/main/java/com/kinx/scheduler/dto/GetPostResponse.java
@@ -0,0 +1,30 @@
+package com.kinx.scheduler.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter
+@Getter
+public class GetPostResponse {
+    @JsonProperty("CategoryId")
+    private Integer categoryId;
+    @JsonProperty("ImgPath")
+    private String imgPath;
+    @JsonProperty("Description")
+    private String description;
+    @JsonProperty("MediaId")
+    private String mediaId;
+    @JsonProperty("ObjectId")
+    private String objectId;
+    @JsonProperty("Alias")
+    private String alias;
+    @JsonProperty("Duration")
+    private String duration;
+    @JsonProperty("Success")
+    private Boolean Success;
+    @JsonProperty("Name")
+    private String name;
+}
